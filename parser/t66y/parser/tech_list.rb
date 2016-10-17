f13b19f613e5417e0e792081e262d4df170366a6
watsy0007 <watsy0007@gmail.com>
2016-10-17 20:33:23 +0800

    improve t66y/parser/tech update file & parser name

--- parser/t66y/parser/tech_list.rb
+++ parser/t66y/parser/tech_list.rb
@@ -1,10 +1,10 @@
 module T66y
   module Parser
-    class Tech
+    class TechList
       include Wombat::Crawler
 
       pages 'xpath=//div[@class=\'pages\']/a', :iterator do |e|
-        e.parser 'tech'
+        e.parser 'tech_list'
         e.link './@href'
       end
     end
